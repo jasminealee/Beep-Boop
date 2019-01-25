4c6db657caa219afd0629cb23137599136e2f390
Jasmine Lee <jasmine.al1722@gmail.com>
2019-01-25 14:17:40 -0800

    revised business logic in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,21 @@
-// var checkDigits = function() {
-//   if ("0") {
-//
-//   }
-// }
-
 $(document).ready(function() {
   $("#beep-boop").submit(function(event) {
 
-    var answersTo = function(user) {
-      if (user % 3 === 0) {
-        return true;
-      } else {
-        return false;
-      }
-    };
 
-     }
     var user = parseInt($("#userInput").val());
+
     var list = [];
+
     for (var i = 0; i <= user; i++) {
       list.push(i);
     }
+
+
+    for (var x = 0; x <= user; x++) {
+      if (list[x] % 3 === 0 && list[x] !== 0) {
+        list.splice(x, 1, "kjhg");
+      }
+    };
 
 
 
