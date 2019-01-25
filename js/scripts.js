87dee1fdb150348612dbf322036a01da757a61a4
Jasmine Lee <jasmine.al1722@gmail.com>
2019-01-25 10:15:18 -0800

    add function string and for statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
+var beepBoop = function(str) {
+  if (str === "0") {
+    return "beep";
+  }
+
+}
+
+
 $(document).ready(function() {
   $("form#beep-boop").submit(function(event) {
 
     var number = parseInt($("input#number").val());
+    var  = $("#userInput").val().split("");
+    for (var i = 0; i < number.length; i++) {
+      if(beepBoop(sentence[i])){
+      sentence[i] = "-";
+    }
+  }
+  var result = sentence.join("");
+  $("#output").append(result);
     var result = beepBoop(number);
     event.preventDefault();
   }
