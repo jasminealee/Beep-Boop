df8b0fd5bc940c24172fcc764f8e385eebffbc65
Jasmine Lee <jasmine.al1722@gmail.com>
2019-01-25 13:16:01 -0800

    add start of an if statement and revisions to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,12 @@
 $(document).ready(function() {
   $("#beep-boop").submit(function(event) {
 
+    if ("0");
+      return "yes";
+
+ }
     var user = parseInt($("#userInput").val());
-
     var list = [];
-
     for (var i = 0; i <= user; i++) {
       list.push(i);
     }
